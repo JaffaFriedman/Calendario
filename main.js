4b8d852a63d80a200c374bd8f3e12eb1fcf6583b
JaffaFriedman <jafriedman.dwfsc4@bootcampudd.cl>
2022-11-02 18:59:18 -0300

    Actualizar plandelmes
    Se agregan copias de calendario

---
+++ main.js
@@ -0,0 +1,8 @@
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+const popover = new bootstrap.Popover('.example-popover', {
+    container: 'body'
+  })
+
+  const exampleEl = document.getElementById('example')
+  const popover = new bootstrap.Popover(exampleEl, options)
